99f62744e24a1203d8a31b5c7fba5ccfc91568b3
Ma1heus <rod.luzmatheus@gmail.com>
2026-09-11 17:01:37 -0300

    feat: created the code for MultiplesOf and RotateListRight as required using only arrays and Lists

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,22 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+
+        // My train of thought
+        // 1. Create an array with length positions;
+        // 2. Create a for to loop through each position of the array;
+        // 3. Add the multiplied numbers into the array;
+        // 4. Return the array;
+
+
+        double[] numbers = new double[length];
+
+        for(int i=0; i < length; i++)
+        {
+            numbers[i] = number * (i + 1); 
+        }
+
+        return numbers;
     }
 
     /// <summary>
@@ -23,11 +38,20 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
+
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // 1. Reverse the entire List;
+        // 2. Reverse the required amount, starting from index 0;
+        // 3. Reverse the rest of the List starting from 'amount' until the end;
+
+        data.Reverse();
+        data.Reverse(0, amount);
+        data.Reverse(amount, data.Count - amount);
     }
 }
